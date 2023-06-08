879d30a78139bb7777e87e65ffc40e0f08d64891
Kalib (Owner) <132502517+ConnellProducts@users.noreply.github.com>
2023-06-08 01:06:00 -0400

    Update gu.py

--- gu.py
+++ gu.py
@@ -1,8 +1,11 @@
 url = "https://users.roblox.com/v1/users/{}"
-isRandom = None
-i = 34
 
-if isRandom != None:
-    i =random.randrange(1, 60000001)
 req=requests.get(url.format(i))
-print(req.json()["displayName"])
+local value = req.json()["displayName"]
+
+def ReplaceLine(n,l,t):
+    with open(n) as f:
+        lines = file.readlins()
+    lines[l] = t
+    
+ReplaceLine("main.lua",1,f"return {value}")
